44c8f91185743fb8bee15ee68208f85871935061
ivil2021 <89508514326@mail.ru>
2021-10-16 22:23:50 +0300

    Header, Navbar, Pofile components were added

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://e7.pngegg.com/pngimages/197/457/png-clipart-round-multicolored-logo-vanamo-logo-icons-logos-emojis-tech-companies.png" />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://wallpaper-mania.com/wp-content/uploads/2018/09/High_resolution_wallpaper_background_ID_77701313431.jpg" />
+            </div>
+            <div>ava + description</div>
+            <div>
+                My posts
+                <div>New post</div>
+                <div>
+                    <div>Post 1</div>
+                </div>
+                <div>
+                    <div>Post 2</div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
