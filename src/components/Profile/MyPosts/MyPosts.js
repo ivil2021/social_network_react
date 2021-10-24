aa1a0eaa96c1516162914ff7c669af64e98671de
ivil2021 <89508514326@mail.ru>
2021-10-24 17:42:13 +0300

    separating UI (user interface) and BLL (bisiness logic layer)

--- src/components/Profile/MyPosts/MyPosts.js
+++ src/components/Profile/MyPosts/MyPosts.js
@@ -3,6 +3,14 @@ import s from './MyPosts.module.css';
 import Post from './Post/Post';
 
 const MyPosts = () => {
+    let postsData = [
+        { id: 1, message: 'how are you', like: 12 },
+        { id: 2, message: 'it is my first project', like: 125 },
+        { id: 3, message: 'qwerty', like: 148 },
+        { id: 4, message: ';xkjfpsf000', like: 178205 },
+        { id: 5, message: 'kofk', like: 15425 },
+    ];
+
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
@@ -15,8 +23,11 @@ const MyPosts = () => {
                 </div>
             </div>
             <div className={s.posts}>
-                <Post message="how are you" like="12" />
-                <Post message="it is my first project" like="125" />
+                <Post message={postsData[0].message} like={postsData[0].like} />
+                <Post message={postsData[1].message} like={postsData[1].like} />
+                <Post message={postsData[2].message} like={postsData[2].like} />
+                <Post message={postsData[3].message} like={postsData[3].like} />
+                <Post message={postsData[4].message} like={postsData[4].like} />
             </div>
         </div>
     );
