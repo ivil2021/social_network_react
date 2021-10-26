1a0cc492f7a784477bd3516c5e117344a73e0a5b
ivil2021 <89508514326@mail.ru>
2021-10-26 10:19:58 +0300

    prolect structure, one more time about ui and bll

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import s from './Dialogs.module.css';
-import { NavLink } from 'react-router-dom';
+// import { NavLink } from 'react-router-dom';
+import DialogItem from './DialogItem/DialogItem';
+import Message from './Message/Message';
 
-const DialogItem = (props) => {
-    let path = '/dialogs/' + props.id;
-    return (
-        <div className={s.dialog + ' ' + s.active}>
-            <NavLink to={path}>{props.name}</NavLink>
-        </div>
-    );
-};
+// const DialogItem = (props) => {
+//     let path = '/dialogs/' + props.id;
+//     return (
+//         <div className={s.dialog + ' ' + s.active}>
+//             <NavLink to={path}>{props.name}</NavLink>
+//         </div>
+//     );
+// };
 
-const Message = (props) => {
-    return <div className={s.dialog}>{props.message}</div>;
-};
+// const Message = (props) => {
+//     return <div className={s.dialog}>{props.message}</div>;
+// };
 
 const Dialogs = () => {
     let dialogs = [
